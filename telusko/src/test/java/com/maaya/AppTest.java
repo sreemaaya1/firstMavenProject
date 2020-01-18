a5dead38a574d72e710cc3b29a05dc3884ae6c4d
maaya <sreemaaya1@gmail.com>
2020-01-19 00:04:07 +0800

    copied maven project

---
+++ telusko/src/test/java/com/maaya/AppTest.java
@@ -0,0 +1,21 @@
+package com.maaya;
+
+import static org.junit.Assert.assertTrue;
+
+import jdk.internal.jline.internal.TestAccessible;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Te
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
